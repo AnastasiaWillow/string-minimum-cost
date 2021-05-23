f3daf6b64e0463f6b281462a623cca25ee6b395a
AnastasiaWillow <84659526+AnastasiaWillow@users.noreply.github.com>
2021-05-23 11:42:22 +0300

    Update string-minimum-cost.cpp

--- string-minimum-cost.cpp
+++ string-minimum-cost.cpp
@@ -52,15 +52,6 @@ int main()
 		}
 	}
 	cout << res << endl;
-
-	cout << endl;
-	
-	for (int i = 0; i < k; i++)
-	{
-		for (int j = 0; j < k; j++)
-			cout << A[i][j] << "\t";
-		cout << "\n";
-	}
 }
 
 bool prov(int number, int**& A, int k)
